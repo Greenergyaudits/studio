9d67ec963ff8d8ab67f79c24a829a623775daf52
Atif Adeel <atif.adeel.1981@gmail.com>
2025-11-10 10:32:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3793518059-d9b4f",
+  "appId": "1:1023807181245:web:b4e82165a69c194fcac2d4",
+  "apiKey": "AIzaSyAUdvdDN5Hk12sEpEJX0pUfcsIiDsJjFSM",
+  "authDomain": "studio-3793518059-d9b4f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1023807181245"
+};
