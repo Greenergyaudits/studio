312720cd9711b26b9a32ba012ce3c8e9eddba684
Atif Adeel <atif.adeel.1981@gmail.com>
2025-11-20 12:19:06 +0000

    ye same masla araha hy

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -25,7 +23,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
